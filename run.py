cd769eb4e52150e2c9776f1ccddb3280889d4d52
CZELYK <ahmethassancelik@gmail.com>
2025-02-25 21:45:45 +0300

    changes have been done on run and test.

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-# run.py
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -13,7 +12,7 @@ from utils.load_vdata import TestData
 class Pscc:
     def __init__(self, args):
         self.device_ids = [Id for Id in range(torch.cuda.device_count())]
-        self.device = torch.device('cuda:0')
+        self.device = torch.device('cuda:0')  # Ensure we are using GPU 0
 
         self.args = args
 
@@ -34,52 +33,58 @@ class Pscc:
         self.SegNet_checkpoint_dir = './checkpoint/{}_checkpoint'.format(self.SegNet_name)
         self.ClsNet_checkpoint_dir = './checkpoint/{}_checkpoint'.format(self.ClsNet_name)
 
+        # Load network weights
         self.load_network_weights()
 
     def load_network_weights(self):
-        # load FENet weight
+        # Load FENet weight
         self.FENet = self.FENet.to(self.device)
         self.FENet = nn.DataParallel(self.FENet, device_ids=self.device_ids)
         self.load_weight(self.FENet, self.FENet_checkpoint_dir, self.FENet_name)
 
-        # load SegNet weight
+        # Load SegNet weight
         self.SegNet = self.SegNet.to(self.device)
         self.SegNet = nn.DataParallel(self.SegNet, device_ids=self.device_ids)
         self.load_weight(self.SegNet, self.SegNet_checkpoint_dir, self.SegNet_name)
 
-        # load ClsNet weight
+        # Load ClsNet weight
         self.ClsNet = self.ClsNet.to(self.device)
         self.ClsNet = nn.DataParallel(self.ClsNet, device_ids=self.device_ids)
         self.load_weight(self.ClsNet, self.ClsNet_checkpoint_dir, self.ClsNet_name)
 
     def load_weight(self, net, checkpoint_dir, name):
         weight_path = '{}/{}.pth'.format(checkpoint_dir, name)
-        net_state_dict = torch.load(weight_path, map_location='cuda:0', weights_only=True)
+        net_state_dict = torch.load(weight_path, map_location=self.device, weights_only=True)
         net.load_state_dict(net_state_dict)
         print('{} weight-loading succeeds'.format(name))
 
     def predict(self, image: torch.Tensor):
+        # Ensure image is on the correct device
         image = image.to(self.device)
 
-        # backbone network
+        # Set the models to evaluation mode
         self.FENet.eval()
-        feat = self.FENet(image)
-
-        # localization head
         self.SegNet.eval()
-        pred_mask = self.SegNet(feat)[0]
-
-        pred_mask = F.interpolate(pred_mask, size=(image.size(2), image.size(3)), mode='bilinear',
-                                  align_corners=True)
-
-        # classification head
         self.ClsNet.eval()
-        pred_logit = self.ClsNet(feat)
 
-        # ce
-        sm = nn.Softmax(dim=1)
-        pred_logit = sm(pred_logit)
-        _, binary_cls = torch.max(pred_logit, 1)
+        with torch.no_grad():
+            # Backbone network (HRNet)
+            feat = self.FENet(image)
 
-        pred_tag = 'forged' if binary_cls.item() == 1 else 'authentic'
+            # Localization head (NLCDetection)
+            pred_mask = self.SegNet(feat)[0]
+            pred_mask = F.interpolate(pred_mask, size=(image.size(2), image.size(3)), mode='bilinear', align_corners=True)
+
+            # Classification head (DetectionHead)
+            pred_logit = self.ClsNet(feat)
+
+            # Softmax for class probabilities
+            sm = nn.Softmax(dim=1)
+            pred_logit = sm(pred_logit)
+
+            # Get predicted class
+            _, binary_cls = torch.max(pred_logit, 1)
+
+            pred_tag = 'forged' if binary_cls.item() == 1 else 'authentic'
+
         return pred_tag
